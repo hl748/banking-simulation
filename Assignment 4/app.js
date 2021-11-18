619ddd3804793814e100b71653699bda58b69d0b
Henry Liu <henryyin12@gmail.com>
2021-11-18 16:28:10 -0500

    changes

--- Assignment 4/app.js
+++ Assignment 4/app.js
@@ -67,6 +67,12 @@ app.get("/test", isAuth, (req,res)=>{
     console.log("testings")
 })
 
-mongoose.connect(MONGODB_URI).then(
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(
     app.listen(4000)
 )
